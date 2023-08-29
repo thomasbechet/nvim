7468dd1dc10f4f35d2c84f94c7ca3ee8260d6a15
Thomas BECHET <thomas.bechet.pro@gmail.com>
2023-08-29 20:32:40 +0200

    neotree + trouble + telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,17 +1,22 @@
 return {
   'nvim-telescope/telescope.nvim', 
   tag = '0.1.2',
-  dependencies = { 'nvim-lua/plenary.nvim' },
-  defaults = {
-    file_ignore_patterns = {
-      'node_modules',
-      '.git/',
-    },
+  dependencies = {
+    'nvim-lua/plenary.nvim',
   },
   config = function()
+    require('telescope').setup({
+      defaults = {
+        file_ignore_patterns = {
+          'node_modules',
+          '.git',
+          'target',
+        },
+      },
+    })
     local builtin = require('telescope.builtin')
     vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-    vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+    -- vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
     vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
     vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
   end
