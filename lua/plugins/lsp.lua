c97edec4db3efccd241d645ed1741daea5f2b59c
Thomas BECHET <thomas.bechet.pro@gmail.com>
2025-01-05 13:36:52 +0100

    change tsserver to ts_ls

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -28,7 +28,7 @@ return {
       }
     })
 
-    lspconfig.tsserver.setup {}
+    lspconfig.ts_ls.setup {}
 
     local capabilities = require('cmp_nvim_lsp').default_capabilities()
     require("lspconfig").clangd.setup({
